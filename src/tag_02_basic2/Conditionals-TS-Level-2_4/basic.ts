1157db7d44861a37f339945f8417ef3a17bebc03
Khrystyna Barshchevska-Kostrova <barshchevskak@gmail.com>
2024-10-01 10:07:52 +0200

    fertig

--- src/tag_02_basic2/Conditionals-TS-Level-2_4/basic.ts
+++ src/tag_02_basic2/Conditionals-TS-Level-2_4/basic.ts
@@ -6,7 +6,7 @@ const y: number = Math.floor(Math.random() * 77) +1;
 console.log(x);
 console.log(y);
 
-if(x == y){
+if(x === y){
     console.log("Was für ein Zufall");
 } else if(x > y){
     console.log("Erste Zahl größer");
@@ -25,3 +25,33 @@ switch (true) {
         console.log("Zweite Zahl größer");
         break;
 }
+
+function randomNumber(){
+    const randomNumber1: number = Math.ceil(Math.random() * 77);
+    const randomNumber2: number = Math.ceil(Math.random() * 77);
+
+    console.log(randomNumber1);
+    console.log(randomNumber2);
+
+    if(randomNumber1 === randomNumber2){
+        console.log("Was für ein Zufall");
+    } else if(randomNumber1 > randomNumber2){
+        console.log("Erste Zahl größer");
+    } else if(randomNumber1 < randomNumber2){
+        console.log("Zweite Zahl größer");
+    }
+
+    switch (true) {
+        case randomNumber1 === randomNumber2:
+            console.log("Was für ein Zufall");
+            break;
+        case randomNumber1 > randomNumber2:
+            console.log("Erste Zahl größer");
+            break;
+        case randomNumber1 < randomNumber2:
+            console.log("Zweite Zahl größer");
+            break;
+    }
+}
+
+randomNumber();
