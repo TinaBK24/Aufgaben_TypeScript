9de07f99dfd3fc900e4d116802b1992f1a2baaaa
Khrystyna Barshchevska-Kostrova <barshchevskak@gmail.com>
2024-10-01 10:20:19 +0200

    fertig

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,1 @@
 import './style.css'
-
-
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="./src/tag_01_intro_ts/Intro-TS-Level-1_8/basic.html"></a>
-    <a href="./src/tag_01_intro_ts/Intro-TS-Level-1_12/basic.html"></a>
-    <a href="./src/tag_01_intro_ts/Intro-TS-Level-1_13/basic.html"></a>
-    <a href="./src/tag_01_intro_ts/Intro-TS-Level-1_16/basic.html"></a>
-    <a href="./src/tag_01_intro_ts/String-Methods-TS-Level-1_1/basic.html"></a>
-    <a href="./src/tag_01_intro_ts/String-Methods-TS-Level-1_2/basic.html"></a>
-    <a href="./src/tag_01_intro_ts/String-Methods-TS-Level-1_3/basic.html"></a>
-    <a href="./src/tag_01_intro_ts/String-Methods-TS-Level-1_5/basic.html"></a>
-    
-  </div>
-`
-
-
