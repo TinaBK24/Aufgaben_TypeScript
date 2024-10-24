cacce7851306cc50abc3d6200a95a89274121b10
Khrystyna Barshchevska-Kostrova <barshchevskak@gmail.com>
2024-10-24 17:09:49 +0200

    tag 18

--- tag_18_oop_II/src/BONUS_OOP-Class-TS-Level-3_1/OOP-Class-TS-Level-3_1.ts
+++ tag_18_oop_II/src/BONUS_OOP-Class-TS-Level-3_1/OOP-Class-TS-Level-3_1.ts
@@ -63,19 +63,19 @@ const actor_3_3 = new Actor(
 
 const episode_1 = new Episode(
     "The Return to Hogwarts", 
-    45, 
+    1, 
     "Harry returns to Hogwarts and struggles against the oppressive regime of Dolores Umbridge while facing the return of Voldemort.", 
     [actor_1_1, actor_1_2, actor_1_3]
 );
 const episode_2 = new Episode(
     "Dumbledore's Army", 
-    45, 
+    2, 
     "Harry forms Dumbledore's Army to teach his friends defense against dark magic while facing challenges from Umbridge.", 
     [actor_2_1, actor_2_2, actor_2_3]
 );
 const episode_3 = new Episode(
     "The Battle of the Department of Mysteries", 
-    48, 
+    3, 
     "The battle at the Department of Mysteries reveals dark secrets about Voldemort's plans and the importance of the prophecy.", 
     [actor_3_1, actor_3_2, actor_3_3]
 );
@@ -88,4 +88,61 @@ const series_1 = new Series(
     [episode_1, episode_2, episode_3]
 );
 
-console.log(series_1);
+const printSeriesInfo = () => {
+    console.log(`
+Title: ${series_1._title}
+Description: ${series_1._description}
+
+Title: ${episode_1._title}
+Description: ${episode_1._description}
+Start Year: ${series_1._startYear}
+End Year: ${series_1._endYear}
+Number of Episodes: ${episode_1._length}
+
+Actors:
+- ${actor_1_1._firstName} ${actor_1_1._lastName}
+    - Birthday: ${actor_1_1._birthday}
+    - Gender: ${actor_1_1._gender}
+- ${actor_1_2._firstName} ${actor_1_2._lastName}
+    - Birthday: ${actor_1_2._birthday}
+    - Gender: ${actor_1_2._gender}
+- ${actor_1_3._firstName} ${actor_1_3._lastName}
+    - Birthday: ${actor_1_3._birthday}
+    - Gender: ${actor_1_3._gender}
+
+Title: ${episode_2._title}
+Description: ${episode_2._description}
+Start Year: ${series_1._startYear}
+End Year: ${series_1._endYear}
+Number of Episodes: ${episode_2._length}
+
+Actors:
+- ${actor_2_1._firstName} ${actor_2_1._lastName}
+    - Birthday: ${actor_2_1._birthday}
+    - Gender: ${actor_2_1._gender}
+- ${actor_2_2._firstName} ${actor_2_2._lastName}
+    - Birthday: ${actor_2_2._birthday}
+    - Gender: ${actor_2_2._gender}
+- ${actor_2_3._firstName} ${actor_2_3._lastName}
+    - Birthday: ${actor_2_3._birthday}
+    - Gender: ${actor_2_3._gender}
+
+Title: ${episode_3._title}
+Description: ${episode_3._description}
+Start Year: ${series_1._startYear}
+End Year: ${series_1._endYear}
+Number of Episodes: ${episode_3._length}
+
+Actors:
+- ${actor_3_1._firstName} ${actor_3_1._lastName}
+    - Birthday: ${actor_3_1._birthday}
+    - Gender: ${actor_3_1._gender}
+- ${actor_3_2._firstName} ${actor_3_2._lastName}
+    - Birthday: ${actor_3_2._birthday}
+    - Gender: ${actor_3_2._gender}
+- ${actor_3_3._firstName} ${actor_3_3._lastName}
+    - Birthday: ${actor_3_3._birthday}
+    - Gender: ${actor_3_3._gender}
+    `);
+}
+printSeriesInfo();
